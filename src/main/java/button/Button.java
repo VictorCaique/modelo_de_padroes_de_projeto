e43a9dd07ef76a402c40cd813043340e60801b65
VictorCaique <60404179+VictorCaique@users.noreply.github.com>
2023-04-10 20:32:17 -0300

    Delete src/main/java directory

--- src/main/java/button/Button.java
+++
@@ -1,7 +0,0 @@
-package button;
-
-// DECLARANDO UMA INTERFACE CHAMADA Button
-public interface Button {
-	// DECLARANDO UM MÉTODO paint() A SER IMPLEMENTADO;
-	void paint();
-}
--- src/main/java/checkbox/Checkbox.java
+++
@@ -1,7 +0,0 @@
-package checkbox;
-
-//DECLARANDO UMA INTERFACE CHAMADA Checkbox
-public interface Checkbox {
-	// DECLARANDO UM MÉTODO paint() A SER IMPLEMENTADO;
-	void paint();
-}
